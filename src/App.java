e47a57441cf852af1c6c7ebd89430e602b40f78f
Fülöp Bence <fulopbencus@gmail.com>
2022-10-05 13:26:21 +0200

    Added counting loop in App

--- src/App.java
+++ src/App.java
@@ -2,5 +2,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("App class is bekoszon!");
         System.out.println("Meg 1x Hello!");
+        for (int i = 1; i < 10; i++) {
+            System.out.println(i+" alma");
+        }
     }
 }
